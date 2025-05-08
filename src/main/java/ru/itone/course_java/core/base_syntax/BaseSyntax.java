28b059713830964a27aba21f3ce245773574a7b2
Байрамов Сергей <sergey.bayramov@gazprombank.ru>
2025-05-08 15:09:08 +0300

    base_syntax: add methods

---
+++ src/main/java/ru/itone/course_java/core/base_syntax/BaseSyntax.java
@@ -0,0 +1,41 @@
+package ru.itone.course_java.core.base_syntax;
+
+public class BaseSyntax {
+
+
+    public int sum(short x, short y) {
+        throw new UnsupportedOperationException();
+    }
+
+    public byte sum(byte x, byte y) {
+        throw new UnsupportedOperationException();
+    }
+
+    public int mult(short x, short y) {
+        throw new UnsupportedOperationException();
+    }
+
+    public long mult(int x, int y) {
+        throw new UnsupportedOperationException();
+    }
+
+    public int div(int x, int y) {
+        throw new UnsupportedOperationException();
+    }
+
+    public int div(long x, long y) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean sameMod(float x, float y) {
+        throw new UnsupportedOperationException();
+    }
+
+    public long sumCharValues(char... x) {
+        throw new UnsupportedOperationException();
+    }
+
+    public double factorial(int x) {
+        throw new UnsupportedOperationException();
+    }
+}
