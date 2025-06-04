3db50fcf5cfe9132a44fd26e8f9460a2c28b44eb
Die <growder1@gmail.com>
2025-06-05 01:45:43 +0500

    Init

---
+++ src/main/java/com/example/itonefirst/ItOneFirstApplication.java
@@ -0,0 +1,13 @@
+package com.example.itonefirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItOneFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItOneFirstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/itonefirst/ItOneFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itonefirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItOneFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
