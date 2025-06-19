b872e8f35932726fc9e19187b3be2e9f52350751
Die <growder1@gmail.com>
2025-06-19 15:31:19 +0500

    Исправил тест findByPersonId и сделал задание

--- src/main/java/ru/itone/course_java/core/base_jdbc/PersonDao.java
+++ src/main/java/ru/itone/course_java/core/base_jdbc/PersonDao.java
@@ -1,9 +1,13 @@
 package ru.itone.course_java.core.base_jdbc;
 
 import lombok.RequiredArgsConstructor;
+import ru.itone.course_java.core.base_jdbc.exceptions.DataAccessException;
 import ru.itone.course_java.core.base_jdbc.model.Person;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * ДАО для {@link Person}
@@ -26,6 +30,27 @@ public class PersonDao {
      * @return Список людей
      */
     public List<Person> getAll() {
-        throw new UnsupportedOperationException();
+        final String sql = "SELECT id, name FROM person";
+
+        try (Connection conn = connection.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            List<Person> persons = new ArrayList<>();
+            while (rs.next()) {
+                persons.add(mapToPerson(rs));
+            }
+            return persons;
+
+        } catch (SQLException e) {
+            throw new DataAccessException("Failed to get all persons", e);
+        }
+    }
+
+    private Person mapToPerson(ResultSet rs) throws SQLException {
+        return new Person(
+                UUID.fromString(rs.getString("id")),
+                rs.getString("name")
+        );
     }
 }
